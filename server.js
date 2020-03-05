d297d9b772b57e7df79adbe3bff590dc7564f9b1
jonariv09 <ingjoseagustinnarvaez@gmail.com>
2020-03-05 16:47:01 -0600

    Creating structure of routes

--- server.js
+++ server.js
@@ -2,24 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const response = require('./network/response')
 const path = require('path')
+const app = express()
 
-var app = express()
+const routes = require('./network/routes')
+
 app.set('port', 3000)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
-app.get('/message', function (req, res) {
-  response.success(req, res, 'Lista de mensajes')
-})
-
-app.post('/', function (req, res) {
-  if (req.query.error == "ok") {
-    response.error(req, res, 'Error simulado', 400)
-  } else {
-    response.success(req, res, 'Creado correctamente', 201)
-  }
-})
+routes(app)
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
